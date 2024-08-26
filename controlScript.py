7be030b6231d6d243de0e764f656cc234ec05c31
Surmi64 <surmi64@gmail.com>
2024-08-26 17:37:08 +0200

    update controlScript.py

--- controlScript.py
+++ controlScript.py
@@ -36,9 +36,13 @@ def get_today_activation_status():
     finally:
         client.close()
 
-def send_command_to_shelly(ip):
-    command = f"curl --anyauth -u {shelly_username}:{shelly_password} http://{ip}/rpc/Script.Start?id=1"
-    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+def send_command_to_shelly(ip,id,action):
+    if action == "start":
+        command = f"curl --anyauth -u {shelly_username}:{shelly_password} http://{ip}/rpc/Script.Start?id={id}"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    elif action == "stop":
+        command = f"curl --anyauth -u {shelly_username}:{shelly_password} http://{ip}/rpc/Script.Stop?id={id}"
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
     if result.returncode != 0:
         logger.error(f"Command failed: {result.stderr}")
@@ -49,19 +53,27 @@ def main():
     should_activate = get_today_activation_status()
     if should_activate.get("water") & should_activate.get("lights"):
         logger.info("Activating Shelly devices...")
-
         try:
-            send_command_to_shelly(shelly1_pm_plus_ip)
-            logger.info("Shelly 1PM Plus activated.")
+            send_command_to_shelly(shelly1_pm_plus_ip,1,"start")
+            logger.info("Water circulation activated for today.")
         except Exception as error:
             logger.error(f"Failed to activate Shelly 1PM Plus: {error}")
         # try:
-        #     send_command_to_shelly(shelly1_plus_ip)
+        #     send_command_to_shelly(shelly1_plus_ip,1,"start")
         #     logger.info("Shelly 1 Plus activated.")
         # except Exception as error:
         #     logger.error(f"Failed to activate Shelly 1 Plus: {error}")
     else:
-        logger.info("No activation needed for today.")
+        try:
+            send_command_to_shelly(shelly1_pm_plus_ip,1,"stop")
+            logger.info("Water circulation deactivated for today.")
+        except Exception as error:
+            logger.error(f"Failed to deactivate Shelly 1PM Plus: {error}")
+        # try:
+        #     send_command_to_shelly(shelly1_plus_ip,1,"stop")
+        #     logger.info("Shelly 1 Plus deactivated.")
+        # except Exception as error:
+        #     logger.error(f"Failed to deactivate Shelly 1 Plus: {error}")
 
 if __name__ == '__main__':
     try:
